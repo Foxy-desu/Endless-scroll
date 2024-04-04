f3fa9c6bc47b99085bbafb6411d3acb743ac3013
Alexandra Orliuk <iammeduza@gmail.com>
2024-04-04 18:04:36 +0300

    feat: implement api for components

---
+++ src/shared/api/index.ts
@@ -0,0 +1,4 @@
+export {Button} from '../button/button';
+export {ContentBlock} from '../content-block/content-block';
+export {Heading} from '../heading/heading';
+export {IdBlock} from '../id-block/id-block';
