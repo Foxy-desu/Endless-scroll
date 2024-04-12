fc1da66647dba5dd438aff37b243c3e6161586ce
Alexandra Orliuk <iammeduza@gmail.com>
2024-04-12 14:45:37 +0300

    feat: add routes

---
+++ src/app/providers/router.tsx
@@ -0,0 +1,14 @@
+import { createBrowserRouter } from "react-router-dom";
+import { MainPage } from "../../pages/main/ui/main-page";
+import { FullPostPageContainer } from "../../pages/full-post/model/full-post-page-container";
+
+export const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <MainPage/>,
+    },
+    {
+      path: '/posts/:id',
+      element: <FullPostPageContainer/>,
+    },
+  ]);
