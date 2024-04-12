252715d0d1fde23fc3c9939e1022fa290fb977c3
Alexandra Orliuk <iammeduza@gmail.com>
2024-04-12 15:01:04 +0300

    feat: create main page component

---
+++ src/pages/main/ui/main-page.tsx
@@ -0,0 +1,10 @@
+import { PostsContainer } from "../../../widgets/posts/api"; 
+import styles from "./main-page.module.scss";
+
+export const MainPage = () => {
+    return (
+        <div className={styles.content}>
+            <PostsContainer/>
+        </div>
+    )
+};
