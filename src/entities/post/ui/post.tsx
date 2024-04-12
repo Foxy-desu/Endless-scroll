0868415897a8951477bb3539a52a1cd0e2063649
Alexandra Orliuk <iammeduza@gmail.com>
2024-04-12 14:58:32 +0300

    fix: remove extra elements for better  component purpose determination

--- src/entities/post/ui/post.tsx
+++ src/entities/post/ui/post.tsx
@@ -1,19 +1,13 @@
-import { Button, ContentBlock, Heading, IdBlock } from '../../../shared/api';
+import { PostId, PostTitle, PostBody } from "../../../shared/api";
+import { IPost } from "../../../shared/types";
 import styles from './post.module.scss';
 
-//decide where to implement post block type
-export const Post = ({postId, heading, postContent, buttonContent, buttonPath}: IPost) => {
+export const Post = ({post, bodyType='expanded' }: {post: IPost, bodyType?:string}): JSX.Element => {
   return (
-    <div className={styles.post}>
-      <article className={styles.post_article}>
-          {postId
-            ? <IdBlock postId = {postId}/>
-            : false
-          }
-          <Heading textContent={heading} />
-          <ContentBlock textContent={postContent}/>
+      <article className={styles.post}>
+              <PostId id={post.id}/>
+              <PostTitle title={post.title}/>
+              <PostBody body={post.body} bodyType={bodyType}/>
       </article>
-      <Button toPath={buttonPath} textContent={buttonContent}/>
-    </div>
   )
-}
+};
